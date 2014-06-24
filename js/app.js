cd2e10a7299c8330bbacd0641939ce3c74da67e2
Diego Moura <mouradsm@ssaiphpserver-desktop>
2014-06-23 21:48:34 -0300

    Ajustes e inicio da automaziação

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 (function(){
     var app = angular.module('lista',[]);
+    var JsonFile = 'Panela.json' //caminho do arquivo json
+
+    function getById(arr, id){
+        for(var i = 0; i < arr.length; i++){
+            var result = arr[i];
+            if(result.id === id){
+                return result;
+            }
+        }
+    }
 
     app.controller('TabController', function(){
 
@@ -17,16 +27,32 @@
     app.controller('SelectController', function($scope, $http){
 
         $scope.Itens = [];
-        $scope.selectedItem = "02";
+        $scope.selectedItens = [];
+        $scope.selectedItem = "";
 
+        $scope.addItemLista = function(id){
+            var item = getById($scope.Itens, id);
+                $scope.selectedItens.push(item);
+                $scope.updateItemStatus(item, 0);
+        }
+
+        $scope.updateItemStatus = function(itemToUpdate, newStatus){
+            if(itemToUpdate)
+                itemToUpdate.status = newStatus;
+        }
+
+
+
+
+
+        //load from JsonFile
         $http({
            method   : 'GET',
-           url      : 'Panela.json'
-
-
+           url      : JsonFile
         }).success(function(result){
            $scope.Itens = result;
-        });
+        })
+
     });
 })();
 
